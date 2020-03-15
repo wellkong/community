2b92c4281446b207d3d148b2e05b4b059e017618
willkong <547860818@qq.com>
2020-03-15 23:43:20 +0800

    h2数据库

---
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -0,0 +1,40 @@
+package life.majiang.community.mapper;
+
+import life.majiang.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * ////////////////////////////////////////////////////////////////////
+ * //                          _ooOoo_                               //
+ * //                         o8888888o                              //
+ * //                         88" . "88                              //
+ * //                         (| ^_^ |)                              //
+ * //                         O\  =  /O                              //
+ * //                      ____/`---'\____                           //
+ * //                    .'  \\|     |//  `.                         //
+ * //                   /  \\|||  :  |||//  \                        //
+ * //                  /  _||||| -:- |||||-  \                       //
+ * //                  |   | \\\  -  /// |   |                       //
+ * //                  | \_|  ''\---/''  |   |                       //
+ * //                  \  .-\__  `-`  ___/-. /                       //
+ * //                ___`. .'  /--.--\  `. . ___                     //
+ * //              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+ * //            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+ * //            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+ * //      ========`-.____`-.___\_____/___.-`____.-'========         //
+ * //                           `=---='                              //
+ * //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+ * //                  佛祖保佑       永不宕机     永无BUG            //
+ * ////////////////////////////////////////////////////////////////////
+ *
+ * @ClassName: UserMapper
+ * @Author: willkong
+ * @Date: 2020/3/13 16:54
+ * @Description: //TODO
+ */
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
