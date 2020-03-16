90589b7fec1c2521048a3e1e1e4aa0ce34bd571f
willkong <547860818@qq.com>
2020-03-17 00:02:09 +0800

    空指针修复

--- src/main/java/life/majiang/community/controller/PublishController.java
+++ src/main/java/life/majiang/community/controller/PublishController.java
@@ -1,13 +1,7 @@
 package life.majiang.community.controller;
 
-import life.majiang.community.mapper.UserMapper;
-import life.majiang.community.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * ////////////////////////////////////////////////////////////////////
@@ -33,32 +27,15 @@ import javax.servlet.http.HttpServletRequest;
  * //                  佛祖保佑       永不宕机     永无BUG            //
  * ////////////////////////////////////////////////////////////////////
  *
- * @ClassName: HelloController
+ * @ClassName: PublishController
  * @Author: willkong
- * @Date: 2020/3/11 11:27
+ * @Date: 2020/3/16 17:53
  * @Description: //TODO
  */
 @Controller
-public class IndexController {
-
-    @Autowired
-    private UserMapper userMapper;
-
-    @GetMapping("/")
-    public String index(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies!=null){
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
-        return "index";
+public class PublishController {
+    @GetMapping("/publish")
+    public String publish(){
+        return "publish";
     }
 }
