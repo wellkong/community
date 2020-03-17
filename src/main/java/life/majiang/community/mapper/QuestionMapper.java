026585b44fbab2e7a339b5429273e6a69ff867b1
willkong J <547860818@qq.com>
2020-03-17 15:40:57 +0800

    add publish

---
+++ src/main/java/life/majiang/community/mapper/QuestionMapper.java
@@ -0,0 +1,40 @@
+package life.majiang.community.mapper;
+
+import life.majiang.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * ////////////////////////////////////////////////////////////////////
+ * //                          _ooOoo_                               //
+ * //                         o8888888o                              //
+ * //                         88" . "88                              //
+ * //                         (| ^_^ |)                              //
+ * //                         O\  =  /O                              //
+ * //                      ____/`---'\____                           //
+ * //                    .'  \\|     |//  `.                         //
+ * //                   /  \\|||  :  |||//  \                        //
+ * //                  /  _||||| -:- |||||-  \                       //
+ * //                  |   | \\\  -  /// |   |                       //
+ * //                  | \_|  ''\---/''  |   |                       //
+ * //                  \  .-\__  `-`  ___/-. /                       //
+ * //                ___`. .'  /--.--\  `. . ___                     //
+ * //              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+ * //            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+ * //            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+ * //      ========`-.____`-.___\_____/___.-`____.-'========         //
+ * //                           `=---='                              //
+ * //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+ * //                  佛祖保佑       永不宕机     永无BUG            //
+ * ////////////////////////////////////////////////////////////////////
+ *
+ * @ClassName: QuesstionMapper
+ * @Author: willkong
+ * @Date: 2020/3/17 11:29
+ * @Description: //TODO
+ */
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,crtator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{crtator},#{tag})")
+    void create(Question question);
+}
---
+++ src/main/java/life/majiang/community/model/Question.java
@@ -0,0 +1,123 @@
+package life.majiang.community.model;
+
+/**
+ * ////////////////////////////////////////////////////////////////////
+ * //                          _ooOoo_                               //
+ * //                         o8888888o                              //
+ * //                         88" . "88                              //
+ * //                         (| ^_^ |)                              //
+ * //                         O\  =  /O                              //
+ * //                      ____/`---'\____                           //
+ * //                    .'  \\|     |//  `.                         //
+ * //                   /  \\|||  :  |||//  \                        //
+ * //                  /  _||||| -:- |||||-  \                       //
+ * //                  |   | \\\  -  /// |   |                       //
+ * //                  | \_|  ''\---/''  |   |                       //
+ * //                  \  .-\__  `-`  ___/-. /                       //
+ * //                ___`. .'  /--.--\  `. . ___                     //
+ * //              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+ * //            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+ * //            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+ * //      ========`-.____`-.___\_____/___.-`____.-'========         //
+ * //                           `=---='                              //
+ * //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+ * //                  佛祖保佑       永不宕机     永无BUG            //
+ * ////////////////////////////////////////////////////////////////////
+ *
+ * @ClassName: Question
+ * @Author: willkong
+ * @Date: 2020/3/17 11:36
+ * @Description: //TODO
+ */
+public class Question {
+    private int id;
+    private String title;
+    private String description;
+    private String tag;
+    private Long gmtCreate;
+    private Long gmtModified;
+    private Integer crtator;
+    private Integer commentCount;
+    private Integer viewCount;
+    private Integer likeCount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Long getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Long gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Long getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Long gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public Integer getCrtator() {
+        return crtator;
+    }
+
+    public void setCrtator(Integer crtator) {
+        this.crtator = crtator;
+    }
+
+    public Integer getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(Integer commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public Integer getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(Integer viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public Integer getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(Integer likeCount) {
+        this.likeCount = likeCount;
+    }
+}
