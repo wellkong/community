f1fb69dfb533cc450b2a848c87cafd4af26784fc
willkong J <547860818@qq.com>
2020-03-18 17:15:01 +0800

    分页

--- src/main/java/life/majiang/community/dto/PageinationDTO.java
+++ src/main/java/life/majiang/community/dto/PageinationDTO.java
@@ -1,14 +1,6 @@
-package life.majiang.community.service;
+package life.majiang.community.dto;
 
-import life.majiang.community.dto.PageinationDTO;
-import life.majiang.community.dto.QuestionDTO;
-import life.majiang.community.mapper.QuestionMapper;
-import life.majiang.community.mapper.UserMapper;
-import life.majiang.community.model.Question;
-import life.majiang.community.model.User;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,43 +29,73 @@ import java.util.List;
  * //                  佛祖保佑       永不宕机     永无BUG            //
  * ////////////////////////////////////////////////////////////////////
  *
- * @ClassName: QuestionService
+ * @ClassName: PageinationDTO
  * @Author: willkong
- * @Date: 2020/3/17 16:51
+ * @Date: 2020/3/18 15:02
  * @Description: //TODO
  */
-@Service
-public class QuestionService {
-    @Autowired
-    private UserMapper userMapper;
-    @Autowired
-    private QuestionMapper questionMapper;
+@Data
+public class PageinationDTO {
+    private List<QuestionDTO> questions;
+    private boolean showPrevious;
+    private boolean showFirstPage;
+    private boolean showNext;
+    private boolean showEndPage;
+    private Integer page;
+    private List<Integer> pages = new ArrayList<>();
+    private Integer totalPage;
 
-    public PageinationDTO list(Integer page, Integer size) {
-        PageinationDTO pageinationDTO = new PageinationDTO();
-        Integer totalCount = questionMapper.count();
-        pageinationDTO.setPagination(totalCount,page,size);
+    public void setPagination(Integer totalCount, Integer page, Integer size) {
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+        } else {
+            totalPage = totalCount / size + 1;
+        }
 
         if (page<1){
             page=1;
         }
-        if (page>pageinationDTO.getTotalPage()){
-            page=pageinationDTO.getTotalPage();
+        if (page>totalPage){
+            page=totalPage;
         }
-        //size*(page-1)
-        Integer offset = size * (page - 1);
-        List<Question> questions = questionMapper.list(offset, size);
-        List<QuestionDTO> questionDTOList = new ArrayList<>();
-        for (Question question : questions) {
-            User user = userMapper.findById(question.getCrtator());
-            if (user != null) {
-                QuestionDTO questionDTO = new QuestionDTO();
-                BeanUtils.copyProperties(question, questionDTO);
-                questionDTO.setUser(user);
-                questionDTOList.add(questionDTO);
+        this.page = page;
+        pages.add(page);
+        for (int i = 1; i <= 3; i++) {
+            if (page - i > 0) {
+                pages.add(0, page - i);
+            }
+
+            if (page + i <= totalPage) {
+                pages.add(page + i);
             }
         }
-        pageinationDTO.setQuestions(questionDTOList);
-        return pageinationDTO;
+
+        //是否展示上一页
+        if (page == 1) {
+            showPrevious = false;
+        } else {
+            showPrevious = true;
+        }
+
+        //是否展示下一页
+        if (page == totalPage) {
+            showNext = false;
+        } else {
+            showNext = true;
+        }
+
+        //是否展示第一页
+        if (pages.contains(1)) {
+            showFirstPage = false;
+        } else {
+            showFirstPage = true;
+        }
+
+        //是否展示最后一页
+        if (pages.contains(totalPage)) {
+            showEndPage = false;
+        } else {
+            showEndPage = true;
+        }
     }
 }
--- src/main/java/life/majiang/community/mapper/QuestionMapper.java
+++ src/main/java/life/majiang/community/mapper/QuestionMapper.java
@@ -3,6 +3,7 @@ package life.majiang.community.mapper;
 import life.majiang.community.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -41,6 +42,8 @@ public interface QuestionMapper {
     @Insert("insert into question (title,description,gmt_create,gmt_modified,crtator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{crtator},#{tag})")
     void create(Question question);
 
-    @Select("select * from question")
-    List<Question> list();
+    @Select("select * from question limit #{offset},#{size}")
+    List<Question> list(@Param("offset") Integer offset, @Param("size") Integer size);
+    @Select("select count(1) from question")
+    Integer count();
 }
