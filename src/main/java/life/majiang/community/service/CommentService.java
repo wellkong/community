cc33d078f6ce6e1357a48118b4b277b6b9a708d4
wellkong <547860818@qq.com>
2020-03-24 19:53:22 +0800

    回复页面

--- src/main/java/life/majiang/community/service/CommentService.java
+++ src/main/java/life/majiang/community/service/CommentService.java
@@ -10,6 +10,7 @@ import life.majiang.community.model.Comment;
 import life.majiang.community.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * ////////////////////////////////////////////////////////////////////
@@ -48,6 +49,8 @@ public class CommentService {
     private QuestionMapper questionMapper;
     @Autowired
     private QuestionExtMapper questionExtMapper;
+
+    @Transactional
     public void insert(Comment comment) {
         if (comment.getParentId() == null || comment.getParentId() == 0){
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
