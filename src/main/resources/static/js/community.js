eff2d8cb359783a49fbdf1f6fbd0505dead6fbbe
wellkong <547860818@qq.com>
2020-03-24 20:21:45 +0800

    提交

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -2,22 +2,29 @@ function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
     $.ajax({
-        type:"POST",
-        url:"/comment",
-        contentType:"application/json",
-        data:JSON.stringify({
-            "parentId":questionId,
-            "content":content,
-            "type":1
+        type: "POST",
+        url: "/comment",
+        contentType: "application/json",
+        data: JSON.stringify({
+            "parentId": questionId,
+            "content": content,
+            "type": 1
         }),
-        success:function (response) {
-            if (response.code === 200){
+        success: function (response) {
+            if (response.code === 200) {
                 $("#comment_section").hide();
-            }else {
-                alert(response.message());
+            } else {
+                if (response.code === 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted){
+                        window.open("https://github.com/login/oauth/authorize?client_id=9c21f9e0227fce6cfe78&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable",true);
+                    }
+                } else {
+                    alert(response.message());
+                }
             }
-            console.log(response);
         },
-        dataType:"json"
+        dataType: "json"
     });
 }
